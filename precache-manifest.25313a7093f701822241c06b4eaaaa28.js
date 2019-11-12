0a23e8f321546d6578ed08886a6bf3b9ab33614f
xyj <593500664@qq.com>
2019-11-12 17:59:25 +0800

    Updates

---
+++ precache-manifest.25313a7093f701822241c06b4eaaaa28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d7cabba6c5b153ff4c82e32637d4cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1946de2d96ddaab0905",
+    "url": "/static/css/2.2c36f076.chunk.css"
+  },
+  {
+    "revision": "bc2af4305407018145a6",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a1946de2d96ddaab0905",
+    "url": "/static/js/2.8f461f7d.chunk.js"
+  },
+  {
+    "revision": "bc2af4305407018145a6",
+    "url": "/static/js/main.4b3bdc1f.chunk.js"
+  },
+  {
+    "revision": "b558e8611826e8e2afdd",
+    "url": "/static/js/runtime-main.4f26494b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
